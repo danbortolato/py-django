0328e40923de430a06bfa173572f1abcfb53673f
Daniel Bortolato <daniel.bortolato@gmail.com>
2022-05-10 03:14:19 -0300

    conf

---
+++ project/settings/__init__.py
@@ -0,0 +1,7 @@
+from .assets import *
+from .databases import *
+from .environment import Path
+from .i18n import *
+from .installed_apps import *
+from .middleware import *
+from .templates import *
