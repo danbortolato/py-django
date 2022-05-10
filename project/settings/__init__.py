0e287f3017ad71203e719d4fa87e4c63a3663503
Daniel Bortolato <daniel.bortolato@gmail.com>
2022-05-10 05:09:31 -0300

    layout-add

--- project/settings/__init__.py
+++
@@ -1,7 +0,0 @@
-from .environment import Path  # isort:skip
-from .installed_apps import * # isort:skip
-from .middleware import * # isort:skip
-from .assets import *
-from .databases import *
-from .i18n import *
-from .templates import *
